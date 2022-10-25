b72d5f6e387caba97776cf1f00d409146a72327b
Md. Majharul Islam <majharul.islam03@northsouth.edu>
2022-10-25 20:59:49 +0600

    firebaseAtZ

---
+++ lib/homepage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Title'),
+      ),
+      body:  Container(
+        child: Center(
+          child: Text("This is container"),
+        ),
+      ),
+    );
+  }
+}
